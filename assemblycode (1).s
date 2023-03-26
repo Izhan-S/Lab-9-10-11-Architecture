313c0317bad5ce0e3c55a9b6335ea0421c05ffe4
Izhan-S <izhansohail@gmail.com>
2023-03-26 14:03:02 +0500

    Initial commit

---
+++ assemblycode (1).s
@@ -0,0 +1,31 @@
+lui gp, 0x4000
+
+addi x1,x0,73     #i=73
+sw x1,0(gp)
+
+addi x2,x0,122    #z=122
+sw x2,0(gp)
+
+addi x3,x0,104    #h = 104
+sw x3,0(gp)
+
+addi x4,x0,97     #a = 97
+sw x4,0(gp)
+
+addi x5,x0,110    #n = 110
+sw x5,0(gp)
+
+addi x6,0,50      #2 = 50
+sw x6,0(gp)
+
+addi x7,0,51      #3 = 51
+sw x7,0(gp)
+
+addi x8,0,49      #1 = 49
+sw x8,0(gp)
+
+addi x9,0,49      #1 = 49
+sw x9,0(gp)
+
+addi x10,0,56     #8 = 56
+sw x10,0(gp)
